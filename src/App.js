ce840ccc78c48ff66ed6b08e427d22cfafa176c5
Chloe <chloe.p.nixon@vanderbilt.edu>
2022-11-23 17:41:44 -0800

    commit of header,footer, meet the team, loading animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import './pages/FrontPage';
 import FrontPage from './pages/FrontPage';
--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,57 +1,76 @@
-import BoardCard from "../components/BoardCard";
+import "./Board.css";
+import "./Pages.css";
+import {Row, Col} from "antd";
+import "antd/dist/antd.css";
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 const people = [
-  {
-    name: "Huntley Guthrie",
-    role: "Director",
-    image: "/assets/huntley.jpg",
-  },
-  {
-    name: "Ryan Roseberry",
-    role: "Vice Director",
-    image: "/assets/ryan.jpg",
-  },
-  {
-    name: "Jessica Brown",
-    role: "Secretary",
-    image: "/assets/jessica.jpg",
-  },
-  {
-    name: "Shawn Gilliland",
-    role: "Treasurer",
-    image: "/assets/shawn.jpg",
-  },
-  {
-    name: "Currently Unnamed",
-    role: "Social Media Coordinator",
-    image: "/assets/currently_unnamed.jpg",
-  },
-  {
-    name: "Stephanie Lee",
-    role: "Fundraising Coordinator",
-    image: "/assets/stephanie.jpg",
-  },
-  {
-    name: "Mike DeRosier",
-    role: "Event Coordinator",
-    image: "/assets/derosier.jpg",
-  },
-  {
-    name: "Mike Stevenson",
-    role: "Event Coordinator",
-    image: "/assets/stevenson.jpg",
-  },
+    {
+        name: "Huntley Guthrie",
+        role: "Director",
+        image: "/assets/huntley.jpg",
+    },
+    {
+        name: "Ryan Roseberry",
+        role: "Vice Director",
+        image: "/assets/ryan.jpg",
+    },
+    {
+        name: "Jessica Brown",
+        role: "Secretary",
+        image: "/assets/jessica.jpg",
+    },
+    {
+        name: "Shawn Gilliland",
+        role: "Treasurer",
+        image: "/assets/shawn.jpg",
+    },
+    {
+        name: "Currently Unnamed",
+        role: "Social Media Coordinator",
+        image: "/assets/currently_unnamed.jpg",
+    },
+    {
+        name: "Stephanie Lee",
+        role: "Fundraising Coordinator",
+        image: "/assets/stephanie.jpg",
+    },
+    {
+        name: "Mike DeRosier",
+        role: "Event Coordinator",
+        image: "/assets/derosier.jpg",
+    },
+    {
+        name: "Mike Stevenson",
+        role: "Event Coordinator",
+        image: "/assets/stevenson.jpg",
+    },
 ];
 
 const Board = () => {
-  return (
-    <div>
-      <h1>Meet the Board</h1>
-      {people.map((person) => (
-        <BoardCard board={person} />
-      ))}
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <div className="bodyParagraph">
+                <h1>Meet the Board</h1>
+                <br></br>
+                <Row gutter={20}>
+                    {people.map((board) => (
+                        <Col span={12}>
+                            <div className="boardcard">
+                                <img className="boardpic" src={board.image}/>
+                                <h2>{board.name}</h2>
+                                <h3>{board.role}</h3>
+                                <br></br>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
+            </div>
+            <Footer/>
+        </div>
+    );
 };
 
 export default Board;
